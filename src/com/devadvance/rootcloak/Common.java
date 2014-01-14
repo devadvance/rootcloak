bb84f018e160e6870c3657f456df7113bbebc97d
devadvance <encryptstream@gmail.com>
2014-01-13 23:58:29 -0500

    Reorganized the code so its more legible. Added a lot of default apps and keywords, fixed the broken activities, and fixed how exec() was being handled (sh -c works a differently than I expected).

--- src/com/devadvance/rootcloak/Common.java
+++ src/com/devadvance/rootcloak/Common.java
@@ -29,8 +29,32 @@ public class Common {
 		"klb.android.lovelive", 
 		"com.incube.epub", 
 		"com.airwatch.androidagent", 
+		"com.zappware.twintv.d3",
+		"com.starfinanz.mobile.android.pushtan",
+		"com.stofa.webtv",
+		"com.barclays.android.barclaysmobilebanking",
+		"com.bskyb.skygo",
+		"com.hanaskcard.rocomo.potal",
+		"com.hanabank.ebk.channel.android.hananbank",
+		"com.ahnlab.v3mobileplus",
+		"com.good.android.gfe",
+		"it.phoenixspa.inbank",
+		"dk.tv2.tv2play",
+		"com.enterproid.divideinstaller",
+		"com.isis.mclient.verizon.activity",
 		"com.isis.mclient.atnt.activity",
-		"com.zappware.twintv.d3"};
+		"com.isis.mclient.atnt.activity",
+		"be.telenet.yelo",
+		"no.rdml.android.mobiletv",
+		"uk.co.barclays.barclayshomeowner",
+		"com.mcafee.apps.emmagent",
+		"com.virginmedia.tvanywhere",
+		"com.amis.mobiatv",
+		"it.telecomitalia.cubovision",
+		"nl.ziggo.android.tv",
+		"com.orange.fr.ocs",
+		"com.adb.android.app.iti",
+		"com.mobileiron"};
 	public static final Set<String> DEFAULT_APPS_SET = new HashSet<String>(Arrays.asList(DEFAULT_APPS_LIST));
 	public static final String DEBUG_KEY = "DEBUGGERPREF";
 	
@@ -39,7 +63,12 @@ public class Common {
 	public static final String KEYWORD_SET_KEY = "KEYWORD_SET";
 	public static final String COMMAND_SET_KEY = "APPS_SET";
 	
-	public static final String[] DEFAULT_KEYWORD_LIST = new String[] { "supersu", "superuser", "Superuser", "noshufou", "xposed", "rootcloak", "chainfire", "titanium"};
+	public static final String[] DEFAULT_KEYWORD_LIST = new String[] { "supersu", "superuser", "Superuser", 
+		"noshufou", "xposed", "rootcloak", 
+		"chainfire", "titanium", "Titanium",
+		"substrate", "greenify", "daemonsu", 
+		"root", "busybox", "titanium",
+		".tmpsu", "su"};
 	public static final Set<String> DEFAULT_KEYWORD_SET = new HashSet<String>(Arrays.asList(DEFAULT_KEYWORD_LIST));
 	public static final String[] DEFAULT_COMMAND_LIST = new String[] { "su", "which", "busybox", "pm", "am", "sh", "ps"};
 	public static final Set<String> DEFAULT_COMMAND_SET = new HashSet<String>(Arrays.asList(DEFAULT_COMMAND_LIST));
--- src/com/devadvance/rootcloak/RootCloak.java
+++ src/com/devadvance/rootcloak/RootCloak.java
@@ -3,8 +3,10 @@ package com.devadvance.rootcloak;
 import static de.robv.android.xposed.XposedHelpers.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -57,6 +59,111 @@ public class RootCloak implements IXposedHookLoadPackage {
 			XposedBridge.log("Loaded app: " + lpparam.packageName);
 		}
 		
+		// Do all of the hooks
+		initOther(lpparam);
+		initFile(lpparam);
+		initPackageManager(lpparam);
+		initActivityManager(lpparam);
+		initRuntime(lpparam);
+		initProcessBuilder(lpparam);
+	}
+	
+	private void initOther(final LoadPackageParam lpparam) {
+		// Always return false when checking if debug is on
+		XposedHelpers.findAndHookMethod("android.os.Debug", lpparam.classLoader, "isDebuggerConnected", XC_MethodReplacement.returnConstant(false));
+
+		// If test-keys, change to release-keys
+		if (!Build.TAGS.equals("release-keys")) {
+			if (debugPref) {
+				XposedBridge.log("Original build tags: " + Build.TAGS);
+			}
+			XposedHelpers.setStaticObjectField(android.os.Build.class, "TAGS", "release-keys");
+			if (debugPref) {
+				XposedBridge.log("New build tags: " + Build.TAGS);
+			}
+		} else {
+			if (debugPref) {
+				XposedBridge.log("No need to change build tags: " + Build.TAGS);
+			}
+		}
+	}
+	
+	private void initFile(final LoadPackageParam lpparam) {
+		// Handles a version of the File constructor. 
+		Constructor<?> constructLayoutParams = findConstructorExact(java.io.File.class, String.class);
+		XposedBridge.hookMethod(constructLayoutParams, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				if (param.args[0] != null){
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor: " + ((String)param.args[0]));
+					}
+				}
+
+				if (((String)param.args[0]).endsWith("su")) {
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor ending with su");
+					}
+					param.args[0] = "/system/xbin/" + FAKE_FILE;
+				} else if (((String)param.args[0]).endsWith("busybox")) {
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor ending with busybox");
+					}
+					param.args[0] = "/system/xbin/" + FAKE_FILE;
+				} else if (stringContainsFromSet(((String)param.args[0]), keywordSet)) {
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor with word super, noshufou, or chainfire");
+					}
+					param.args[0] = "/system/app/" + FAKE_FILE + ".apk";
+				}
+			}
+		});
+		
+		// Another version of the File constructor.
+		Constructor<?> extendedFileConstructor = findConstructorExact(java.io.File.class, String.class, String.class);
+		XposedBridge.hookMethod(extendedFileConstructor, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				if (param.args[0] != null && param.args[1] != null){
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor: " + ((String)param.args[0]) + ", with: "+ ((String)param.args[1]));
+					}
+				}
+
+				if (((String)param.args[1]).equalsIgnoreCase("su")) {
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor with filename su");
+					}
+					param.args[1] = FAKE_FILE;
+				} else if (((String)param.args[1]).contains("busybox")) {
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor ending with busybox");
+					}
+					param.args[1] = FAKE_FILE;
+				} else if (stringContainsFromSet(((String)param.args[1]), keywordSet)) {
+					if (debugPref) {
+						XposedBridge.log("File: Found a File constructor with word super, noshufou, or chainfire");
+					}
+					param.args[1] = FAKE_FILE + ".apk";
+				}
+			}
+		});
+		
+		// Currently just for debugging purposes, not normally used
+		Constructor<?> uriFileConstructor = findConstructorExact(java.io.File.class, URI.class);
+		XposedBridge.hookMethod(uriFileConstructor, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
+			@Override
+			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+				if (param.args[0] != null){
+					if (debugPref) {
+						XposedBridge.log("File: Found a URI File constructor: " + ((URI)param.args[0]).toString());
+					}
+				}
+			}
+		});
+	}
+	
+	private void initPackageManager(final LoadPackageParam lpparam) {
 		// Hooks getInstalledApplications. For this method we will remove any keywords, such as supersu and superuser, out of the result list.
 		findAndHookMethod("android.app.ApplicationPackageManager", lpparam.classLoader, "getInstalledApplications", int.class, new XC_MethodHook() {
 			@SuppressWarnings("unchecked")
@@ -110,7 +217,9 @@ public class RootCloak implements IXposedHookLoadPackage {
 				param.setResult(packages);
 			}
 		});
-		
+	}
+	
+	private void initActivityManager(final LoadPackageParam lpparam) {
 		// Hooks getPackageInfo. For this method we will prevent the package info from being obtained for any app in the list
 		findAndHookMethod("android.app.ApplicationPackageManager", lpparam.classLoader, "getPackageInfo", String.class, int.class, new XC_MethodHook() {
 			@Override
@@ -210,7 +319,9 @@ public class RootCloak implements IXposedHookLoadPackage {
 				param.setResult(processes);
 			}
 		});
-
+	}
+	
+	private void initRuntime(final LoadPackageParam lpparam) {
 		// Hooks the Runtime.exec() method. This is the only one that needs to be hooked because the other two versions of exec() just end up calling this one.
 		findAndHookMethod("java.lang.Runtime", lpparam.classLoader, "exec", String[].class, String[].class, File.class, new XC_MethodHook() {
 			@Override
@@ -234,50 +345,43 @@ public class RootCloak implements IXposedHookLoadPackage {
 							XposedBridge.log("Found blacklisted command at the end of the string: " + firstParam);
 						}
 						
-						if (commandSet.contains("pm") && firstParam.endsWith("pm")) {
+						if (firstParam.equals("su") || firstParam.endsWith("/su")) { // If its su or ends with su (/bin/su, /xbin/su, etc)
+							param.setThrowable(new IOException());
+						} else if (commandSet.contains("pm") && (firstParam.equals("pm") || firstParam.endsWith("/pm"))) {
 							if (execArray.length >= 3 && execArray[1].equalsIgnoreCase("list") && execArray[2].equalsIgnoreCase("packages")) {
 								// If getting list of packages, exclude anything with su
-								param.args[0] = new String[] {"pm", "list", "packages", "-v", "grep", "-v", "\"su\""};
+								//param.args[0] = new String[] {"pm", "list", "packages", "-v", "grep", "-v", "\"su\""};
+								param.args[0] = buildGrepArraySingle(execArray, true);
 							} else if (execArray.length >= 3 && (execArray[1].equalsIgnoreCase("dump") || execArray[1].equalsIgnoreCase("path"))) {
 								// If getting dumping or getting the path, don't let it work if it contains any of the keywords
 								if (stringContainsFromSet(execArray[2], keywordSet)) {
 									param.args[0] = new String[] {execArray[0], execArray[1], FAKE_PACKAGE};
 								}
 							}
-						} else if (commandSet.contains("ps") && firstParam.endsWith("ps")) { // This is a process list command
-							param.args[0] = new String[] {"ps", "|", "grep", "-v", "\"su\""};
-						} else if (firstParam.endsWith("su")) {
-							execArray[0] = FAKE_COMMAND;
-							param.args[0] = execArray;
-						} else if (commandSet.contains("which") && firstParam.endsWith("which")) { // This is a busybox which command
-							if ((execArray.length >= 2)) {
-								execArray[0] = "/system/xbin/" + FAKE_COMMAND;
-								param.args[0] = execArray;
-							}
+						} else if (commandSet.contains("ps") && (firstParam.equals("ps") || firstParam.endsWith("/ps"))) { // This is a process list command
+							//param.args[0] = new String[] {"ps", "|", "grep", "-v", "\"su\""};
+							param.args[0] = buildGrepArraySingle(execArray, true);
+						} else if (commandSet.contains("which") && (firstParam.equals("which") || firstParam.endsWith("/which"))) { // This is a busybox which command
+							param.setThrowable(new IOException());
 						} else if (commandSet.contains("busybox")){
 							for (String tempString : execArray) {
 								if (tempString.endsWith("busybox")) {
-									param.args[0] = new String[] {FAKE_COMMAND, FAKE_COMMAND};
-									break;
+									param.setThrowable(new IOException());
 								}
 							}
-						} else if (commandSet.contains("sh") && firstParam.endsWith("sh")) {
-							execArray[0] = "/system/xbin/" + FAKE_COMMAND;
-							param.args[0] = execArray;
+						} else if (commandSet.contains("sh") && (firstParam.equals("sh") || firstParam.endsWith("/sh"))) {
+							param.setThrowable(new IOException());
 						} else {
-							execArray[0] = "/system/xbin/" + FAKE_COMMAND;
-							param.args[0] = execArray;
+							param.setThrowable(new IOException());
 						}
-
-						if (debugPref) {
+						
+						if (debugPref && param.getThrowable() == null) {
 							String tempString = "New Exec Command:";
 							for (String temp : (String[])param.args[0]) {
 								tempString = tempString + " " + temp;
 							}
 							XposedBridge.log(tempString);
 						}
-						
-						
 					}
 
 					
@@ -288,99 +392,9 @@ public class RootCloak implements IXposedHookLoadPackage {
 				}
 			}
 		});
-
-		// Handles a version of the File constructor. 
-		Constructor<?> constructLayoutParams = findConstructorExact(java.io.File.class, String.class);
-		XposedBridge.hookMethod(constructLayoutParams, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				if (param.args[0] != null){
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor: " + ((String)param.args[0]));
-					}
-				}
-
-				if (((String)param.args[0]).endsWith("su")) {
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor ending with su");
-					}
-					param.args[0] = "/system/xbin/" + FAKE_FILE;
-				} else if (((String)param.args[0]).endsWith("busybox")) {
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor ending with busybox");
-					}
-					param.args[0] = "/system/xbin/" + FAKE_FILE;
-				} else if (stringContainsFromSet(((String)param.args[0]), keywordSet)) {
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor with word super, noshufou, or chainfire");
-					}
-					param.args[0] = "/system/app/" + FAKE_FILE + ".apk";
-				}
-			}
-		});
-		
-		// Another version of the File constructor.
-		Constructor<?> extendedFileConstructor = findConstructorExact(java.io.File.class, String.class, String.class);
-		XposedBridge.hookMethod(extendedFileConstructor, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				if (param.args[0] != null && param.args[1] != null){
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor: " + ((String)param.args[0]) + ", with: "+ ((String)param.args[1]));
-					}
-				}
-
-				if (((String)param.args[1]).equalsIgnoreCase("su")) {
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor with filename su");
-					}
-					param.args[1] = FAKE_FILE;
-				} else if (((String)param.args[1]).contains("busybox")) {
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor ending with busybox");
-					}
-					param.args[1] = FAKE_FILE;
-				} else if (stringContainsFromSet(((String)param.args[1]), keywordSet)) {
-					if (debugPref) {
-						XposedBridge.log("File: Found a File constructor with word super, noshufou, or chainfire");
-					}
-					param.args[1] = FAKE_FILE + ".apk";
-				}
-			}
-		});
-		
-		// Currently just for debugging purposes, not normally used
-		Constructor<?> uriFileConstructor = findConstructorExact(java.io.File.class, URI.class);
-		XposedBridge.hookMethod(uriFileConstructor, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
-			@Override
-			protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-				if (param.args[0] != null){
-					if (debugPref) {
-						XposedBridge.log("File: Found a URI File constructor: " + ((URI)param.args[0]).toString());
-					}
-				}
-			}
-		});
-
-
-		// Always return false when checking if debug is on
-		XposedHelpers.findAndHookMethod("android.os.Debug", lpparam.classLoader, "isDebuggerConnected", XC_MethodReplacement.returnConstant(false));
-
-		// If test-keys, change to release-keys
-		if (!Build.TAGS.equals("release-keys")) {
-			if (debugPref) {
-				XposedBridge.log("Original build tags: " + Build.TAGS);
-			}
-			XposedHelpers.setStaticObjectField(android.os.Build.class, "TAGS", "release-keys");
-			if (debugPref) {
-				XposedBridge.log("New build tags: " + Build.TAGS);
-			}
-		} else {
-			if (debugPref) {
-				XposedBridge.log("No need to change build tags: " + Build.TAGS);
-			}
-		}
-
+	}
+	
+	private void initProcessBuilder(final LoadPackageParam lpparam) {
 		// Hook ProcessBuilder and prevent running of certain commands
 		Constructor<?> processBuilderConstructor2 = findConstructorExact(java.lang.ProcessBuilder.class, String[].class);
 		XposedBridge.hookMethod(processBuilderConstructor2, new XC_MethodHook(XCallback.PRIORITY_HIGHEST) {
@@ -411,7 +425,6 @@ public class RootCloak implements IXposedHookLoadPackage {
 				}
 			}
 		});
-		
 	}
 
 	public void loadPrefs() {
@@ -471,4 +484,25 @@ public class RootCloak implements IXposedHookLoadPackage {
 		return false;
 	}
 
+	private String[] buildGrepArraySingle(String[] original, boolean addSH) {
+		StringBuilder builder = new StringBuilder();
+		ArrayList<String> originalList = new ArrayList<String>();
+		if (addSH) {
+			originalList.add("sh");
+			originalList.add("-c");
+		}
+		for (String temp : original) {
+			builder.append(" ");
+			builder.append(temp);
+		}
+		//originalList.addAll(Arrays.asList(original));
+		for (String temp : keywordSet) {
+			builder.append(" | grep -v ");
+			builder.append(temp);
+		}
+		//originalList.addAll(Common.DEFAULT_GREP_ENTRIES);
+		originalList.add(builder.toString());
+		return originalList.toArray(new String[0]);
+	}
+	
 }
